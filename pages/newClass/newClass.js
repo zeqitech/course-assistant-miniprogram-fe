0964f0b45eac3f6847fb874cef3135b0d3a1a8fc
syyCN <sunyiyangcn@outlook.com>
2021-03-26 11:32:37 +0800

    add picker to newClass page

--- pages/newClass/newClass.js
+++ pages/newClass/newClass.js
@@ -1,12 +1,25 @@
+import { classTimeArray } from './data';
+
 Page({
   /**
    * 页面初始数据
    */
-  data: {},
+  data: {
+    name: '', //班级名称
+    time: '', //上课时间
+    timeIndex: [0, 0, 0, 0, 0], //上课时间索引
+    classTimeArray,
+  },
 
   /**
    * 页面加载生命周期函数
    * @param {Object} options
    */
-  onLoad(options) {},
+  onLoad(options) {
+    console.log(this.data.classTimeArray);
+  },
+
+  handleSelectClassTime(e) {
+    console.log(e);
+  },
 });
