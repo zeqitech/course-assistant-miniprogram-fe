3974da037dce7e17e26d230320131e2a90ce63b2
syyCN <sunyiyangcn@outlook.com>
2021-03-26 22:49:36 +0800

    add scard component

---
+++ components/scard/scard.js
@@ -0,0 +1,27 @@
+Component({
+  /**
+   * 组件初始数据
+   */
+  properties: {
+    cover: {
+      type: String,
+      value: '',
+    },
+    title: {
+      type: String,
+      value: '',
+    },
+    time: {
+      type: String,
+      value: '',
+    },
+    member: {
+      type: String,
+      value: '0',
+    },
+    state: {
+      type: String,
+      value: '',
+    },
+  },
+});
