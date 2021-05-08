63b69eb8abdc4f3e0e6d0f306c0f1358db9d2740
syyCN <sunyiyangcn@outlook.com>
2021-05-08 13:46:11 +0800

    improve signIn page

--- pages/sign/signIn/signIn.js
+++ pages/sign/signIn/signIn.js
@@ -1,9 +1,12 @@
+const app = getApp().globalData;
+
 Page({
   /**
    * 页面初始数据
    */
   data: {
-    token: '',
+    // 课程 ID
+    courseId: '',
   },
 
   /**
@@ -15,53 +18,65 @@ Page({
     console.log(options);
     console.log('----------------------------------------------');
     this.setData({
-      token: options.token,
+      courseId: '',
     });
   },
 
   /**
    * 处理签到事件
    */
-  handleSignIn() {
+  async handleSignIn() {
+    // 显示 Loading
     tt.showToast({
       title: '定位中',
       icon: 'loading',
     });
-    tt.getLocation({
-      type: 'gcj02',
-      success: (res) => {
-        console.log(res);
-        var latitude = res.latitude;
-        var longitude = res.longitude;
-        tt.request({
-          url: 'https://baidu.com',
-          data: {
-            user_name: 'hello',
-          },
-          header: {
-            'content-type': 'application/json',
-          },
-          success: (res) => {
-            console.log(`请求发送成功`);
-            tt.hideLoading({
-              success(res) {
-                console.log(`res`);
-                tt.showModal({
-                  title: '签到成功',
-                  content: '请继续保持良好表现！',
-                  icon: 'success',
-                });
-              },
-            });
-          },
-          fail(res) {
-            console.log(`request 调用失败`);
-          },
-        });
-      },
-      fail(res) {
-        console.log('定位失败');
-      },
+    // 获取位置
+    let location = await new Promise((resolve) => {
+      tt.getLocation({
+        type: 'gcj02',
+        complete(res) {
+          // 回传数据
+          resolve(res);
+        },
+      });
     });
+    // 发送签到请求
+    let signInRes = await new Promise((resolve) => {
+      tt.request({
+        url: app.urlConfig.signInUrl,
+        method: 'POST',
+        data: {
+          courseId: this.data.courseId,
+          studentId: app.openId,
+          latitude: location.latitude.toString(),
+          longitude: location.longitude.toString(),
+        },
+        header: {
+          'content-type': 'application/json',
+        },
+        complete(res) {
+          resolve(res.data);
+        },
+      });
+    });
+    // 签到成功
+    if (signInRes.success) {
+      tt.showModal({
+        title: '成功',
+        content: '签到成功',
+        success() {
+          tt.navigateBack({
+            delta: 1,
+          });
+        },
+      });
+    } else {
+      // 签到失败
+      tt.showModal({
+        title: '失败',
+        content: signInRes.message,
+      });
+    }
   },
 });
